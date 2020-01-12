ec1b127c63eab79a139bfb1e2fe0b26f4fe4368e
alvarobahrain <51244615+alvarobahrain@users.noreply.github.com>
2020-01-12 13:12:00 +0000

    Add files via upload

--- assert.js
+++ assert.js
@@ -1,55 +1,107 @@
-"use strict";
-module.exports = (function(){
-var AssertionError = (function() {
-    function AssertionError(a) {
-        this.constructor$(a);
-        this.message = a;
-        this.name = "AssertionError";
+/*
+ * assert.js: Assertion helpers for broadway tests
+ *
+ * (C) 2011, Nodejitsu Inc.
+ * MIT LICENSE
+ *
+ */
+
+var assert = module.exports = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    nconf = require('nconf'),
+    vows = require('vows');
+
+//
+// ### Assertion helpers for working with `broadway.App` objects.
+//
+assert.app = {};
+
+//
+// ### Assertion helpers for working with `broadway.plugins`.
+//
+assert.plugins = {};
+
+//
+// ### Assert that an application has various plugins.
+//
+assert.plugins.has = {
+  config: function (app, config) {
+    assert.instanceOf(app.config, nconf.Provider);
+    if (config) {
+      //
+      // TODO: Assert that all configuration has been loaded
+      //
     }
-    AssertionError.prototype = new Error();
-    AssertionError.prototype.constructor = AssertionError;
-    AssertionError.prototype.constructor$ = Error;
-    return AssertionError;
-})();
+  },
+  exceptions: function (app) {
 
-function getParams(args) {
-    var params = [];
-    for (var i = 0; i < args.length; ++i) params.push("arg" + i);
-    return params;
-}
-
-function nativeAssert(callName, args, expect) {
-    try {
-        var params = getParams(args);
-        var constructorArgs = params;
-        constructorArgs.push("return " +
-                callName + "("+ params.join(",") + ");");
-        var fn = Function.apply(null, constructorArgs);
-        return fn.apply(null, args);
-    } catch (e) {
-        if (!(e instanceof SyntaxError)) {
-            throw e;
-        } else {
-            return expect;
-        }
+  },
+  directories: function (app) {
+    if (app.options['directories']) {
+      Object.keys(app.options['directories']).forEach(function (key) {
+        assert.isTrue((fs.existsSync || path.existsSync)(app.options['directories'][key]));
+      });
     }
-}
+  },
+  log: function (app) {
+    assert.isObject(app.log);
 
-return function assert(boolExpr, message) {
-    if (boolExpr === true) return;
-
-    if (typeof boolExpr === "string" &&
-        boolExpr.charAt(0) === "%") {
-        var nativeCallName = boolExpr;
-        var $_len = arguments.length;var args = new Array(Math.max($_len - 2, 0)); for(var $_i = 2; $_i < $_len; ++$_i) {args[$_i - 2] = arguments[$_i];};
-        if (nativeAssert(nativeCallName, args, message) === message) return;
-        message = (nativeCallName + " !== " + message);
-    }
-
-    var ret = new AssertionError(message);
-    if (Error.captureStackTrace) {
-        Error.captureStackTrace(ret, assert);
-    }
-    throw ret;
+    //
+    // TODO: Assert winston.extend methods
+    //
+  }
 };
-})();
+
+//
+// ### Assert that an application doesn't have various plugins
+//
+assert.plugins.notHas = {
+  config: function (app) {
+    assert.isTrue(!app.config);
+  },
+  exceptions: function (app) {
+
+  },
+  directories: function (app) {
+    assert.isTrue(!app.config.get('directories'))
+  },
+  log: function (app) {
+    assert.isTrue(!app.log);
+    //
+    // TODO: Assert winston.extend methods
+    //
+  }
+};
+
+assert.log = {};
+
+assert.log.levelMsgMeta = function (err, level, msg, meta) {
+  assert.equal(level, this.event[1]);
+  assert.equal(msg, this.event[2]);
+  assert.equal(meta, this.event[3]);
+};
+
+assert.log.msgMeta = function (err, level, msg, meta) {
+  assert.equal(level, this.event[0].split('::')[1] || 'info');
+  assert.equal(msg, this.event[1]);
+  assert.equal(meta, this.event[2]);
+};
+
+assert.log.levelMeta = function (err, level, msg, meta) {
+  assert.equal(level, this.event[1]);
+  assert.equal(msg, this.event[0]);
+  assert.deepEqual(meta, this.event[2]);
+};
+
+assert.log.levelMsg = function (err, level, msg, meta) {
+  assert.equal(level, this.event[1]);
+  assert.equal(msg, this.event[2]);
+};
+
+assert.log.metaOnly = function (err, level, msg, meta, event) {
+  assert.equal(level, 'info');
+  assert.equal(msg, this.event[0]);
+  assert.equal(meta, this.event[1]);
+  assert.equal(event, this.event[0]);
+};
